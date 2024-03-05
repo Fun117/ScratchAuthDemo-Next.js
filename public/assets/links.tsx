f46f83ff8c9c564bd26cb73ec2eca3144fb57814
Fun117 <fun117.kun@gmail.com>
2024-03-06 00:47:10 +0900

    first commit

--- public/assets/links.tsx
+++ public/assets/links.tsx
@@ -1,4 +1,4 @@
 export const links_config = {
-    "site_origin": "https://scratch-auth-demo-ts.vercel.app",
+    "site_origin": "https://scratch-auth-demo-nextjs.vercel.app",
     "site_title": "Scratch Auth Demo for Next.js",
 }
